7ed9a5c1fa37de52d07964adcbff2011879c99ff
Kobe <danrok19@wp.pl>
2022-11-26 16:06:28 +0100

    naprawa bledow

--- server.js
+++ server.js
@@ -28,25 +28,25 @@ app.get('/meals', (req, res) => {
     });
 });
 
-app.get('/meals/:index_nr', (req, res) => {
-    fs.readFile('./meals.json', 'utf8', (err, mealsJson) => {
-        if (err) {
-            console.log("File read failed in GET /meals/" + req.params.index_nr + ": "+ err);
-            res.status(500).send('File read failed');
-            return;
-        }
-        var meals = JSON.parse(mealsJson);
-        var meal = meals.find(mealtmp => mealtmp.index_nr == req.params.index_nr);
-        if (!meal) {
-            console.log("Can't find meal with id: " + req.params.index_nr);
-            res.status(500).send('Cant find meal with id: ' + req.params.index_nr);
-            return;
-        }
-        var mealJSON = JSON.stringify(meal);
-        console.log("GET /meals/" + req.params.index_nr);
-        res.send(mealJSON);
-    });
-});
+// app.get('/meals/:index_nr', (req, res) => {
+//     fs.readFile('./meals.json', 'utf8', (err, mealsJson) => {
+//         if (err) {
+//             console.log("File read failed in GET /meals/" + req.params.index_nr + ": "+ err);
+//             res.status(500).send('File read failed');
+//             return;
+//         }
+//         var meals = JSON.parse(mealsJson);
+//         var meal = meals.find(mealtmp => mealtmp.index_nr == req.params.index_nr);
+//         if (!meal) {
+//             console.log("Can't find meal with id: " + req.params.index_nr);
+//             res.status(500).send('Cant find meal with id: ' + req.params.index_nr);
+//             return;
+//         }
+//         var mealJSON = JSON.stringify(meal);
+//         console.log("GET /meals/" + req.params.index_nr);
+//         res.send(mealJSON);
+//     });
+// });
 //POST
 app.post('/meals', (req, res) => {
     fs.readFile('./meals.json', 'utf8', (err, mealsJson) => {
@@ -66,12 +66,12 @@ app.post('/meals', (req, res) => {
                     res.status(500).send('Error writing file meals.json');
                 } else {
                     res.status(201).send(req.body);
-                    console.log("Successfully wrote file meals.json and added new meal with id = " + req.body.index_nr);
+                    console.log("Successfully wrote file meals.json and added new meal with index_nr = " + req.body.index_nr);
                 }
             });
         } else {
-            console.log("Meal by id = " + req.body.index_nr + " already exists");
-            res.status(500).send('Meal by id = ' + req.body.index_nr + ' already exists');
+            console.log("Meal by index_nr = " + req.body.index_nr + " already exists");
+            res.status(500).send('Meal by index_nr = ' + req.body.index_nr + ' already exists');
             return;
         }
     });
@@ -88,8 +88,8 @@ app.put('/meals/:index_nr', (req, res) => {
         var meals = JSON.parse(mealsJson);
         var mealBody = meals.find(mealtmp => mealtmp.index_nr == req.body.index_nr);
         if (mealBody && mealBody.index_nr != req.params.index_nr) {
-            console.log("Meal by id = " + mealBody.index_nr + " already exists");
-            res.status(500).send('Meal by id = ' + mealBody.index_nr + ' already exists');
+            console.log("Meal by index_nr = " + mealBody.index_nr + " already exists");
+            res.status(500).send('Meal by index_nr = ' + mealBody.index_nr + ' already exists');
             return;
         }
         var meal = meals.find(mealtmp => mealtmp.index_nr == req.params.index_nr);
@@ -102,7 +102,7 @@ app.put('/meals/:index_nr', (req, res) => {
                     res.status(500).send('Error writing file meals.json');
                 } else {
                     res.status(201).send(req.body);
-                    console.log("Successfully wrote file meals.json and added new meal with id = " + req.body.index_nr);
+                    console.log("Successfully wrote file meals.json and added new meal with index_nr = " + req.body.index_nr);
                 }
             });
         } else {
@@ -118,13 +118,41 @@ app.put('/meals/:index_nr', (req, res) => {
                     res.status(500).send('Error writing file orders.json');
                 } else {
                     res.status(200).send(req.body);
-                    console.log("Successfully wrote file meals.json and edit order with old id = " + req.params.index_nr);
+                    console.log("Successfully wrote file meals.json and edit order with old index_nr = " + req.params.index_nr);
                 }
             });
         }
     });
 });
 
+app.delete('/meals/:id', (req, res) => {
+    fs.readFile('./meals.json', 'utf8', (err, mealsJson) => {
+        if (err) {
+            console.log("File read failed in DELETE /meals: "+ err);
+            res.status(500).send('File read failed');
+            return;
+        }
+        var meals = JSON.parse(mealsJson);
+        var mealIndex = meals.findIndex(mealtmp => mealtmp.mealId == req.params.id);
+        if (orderIndex != -1) {
+            orders.splice(orderIndex, 1);
+            var newList = JSON.stringify(orders);
+            fs.writeFile('./orders.json', newList, err => {
+                if (err) {
+                    console.log("Error writing file in DELETE /orders/" + req.params.id+": "+ err);
+                    res.status(500).send('Error writing file orders.json');
+                } else {
+                    res.status(204).send();
+                    console.log("Successfully deleted order with id = " + req.params.id);
+                }
+            });
+        } else {
+            console.log("Order by id = " + req.params.id + " does not exists");
+            res.status(500).send('Order by id = ' + req.params.id + ' does not exists');
+            return;
+        }
+    });
+});
 
 
 
