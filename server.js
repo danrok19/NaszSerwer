ea397061846cff119f8c1409e7fba01363ba68cf
Kobe <danrok19@wp.pl>
2022-11-27 15:55:21 +0100

    dodanie PUT meal

--- server.js
+++ server.js
@@ -242,3 +242,50 @@ app.delete('/categories/:index_nr', (req, res) => {
         }
     });
 });
+
+app.put('/categories/:index_nr', (req, res) => {
+    fs.readFile('./categories.json', 'utf8', (err, categoriesJson) => {
+        if (err) {
+            console.log("File read failed in PUT /categories/" + req.params.index_nr+": "+ err);
+            res.status(500).send('File read failed');
+            return;
+        }
+        var categories = JSON.parse(categoriesJson);
+        var categoryBody = categories.find(categorytmp => categorytmp.index_nr == req.body.index_nr);
+        if (categoryBody && categoryBody.index_nr != req.params.index_nr) {
+            console.log("Category by index_nr = " + categoryBody.index_nr + " already exists");
+            res.status(500).send('Category by index_nr = ' + categoryBody.index_nr + ' already exists');
+            return;
+        }
+        var category = categories.find(categorytmp => categorytmp.index_nr == req.params.index_nr);
+        if (!category) {
+            categories.push(req.body);
+            var newList = JSON.stringify(categories);
+            fs.writeFile('./categories.json', newList, err => {
+                if (err) {
+                    console.log("Error writing file in PUT /categories/" + req.params.index_nr+": "+err);
+                    res.status(500).send('Error writing file categories.json');
+                } else {
+                    res.status(201).send(req.body);
+                    console.log("Successfully wrote file categories.json and added new category with index_nr = " + req.body.index_nr);
+                }
+            });
+        } else {
+            for (var i = 0; i < categories.length; i++) {
+                if (categories[i].index_nr == category.index_nr) {
+                    categories[i] = req.body;
+                }
+            }
+            var newList = JSON.stringify(categories);
+            fs.writeFile('./categories.json', newList, err => {
+                if (err) {
+                    console.log("Error writing file in PUT /categories/" + req.params.index_nr+": "+ err);
+                    res.status(500).send('Error writing file orders.json');
+                } else {
+                    res.status(200).send(req.body);
+                    console.log("Successfully wrote file categories.json and edit order with old index_nr = " + req.params.index_nr);
+                }
+            });
+        }
+    });
+});
